f7700f5d07f6e2d839886a021a76a99633a9c369
aditya-s1234 <adityasubramanian16@gmail.comp>
2026-08-13 18:55:33 -0700

    Created frontend addition/subtraction accessibility, improved error handling for matrix arithmetic

--- Application.cpp
+++ Application.cpp
@@ -59,7 +59,8 @@ void Application::create() {
     while (true) {
         int inp{};
         std::cout << "[1] Create matrix \n"
-                     "[2] View matrix \n";
+                     "[2] View matrix \n"
+                     "[3] Return \n";
         std::cin >> inp;
         switch (inp) {
             case 1: {
@@ -81,9 +82,15 @@ void Application::create() {
                     int index{};
                     std::cout << "Enter matrix you want to view (index): ";
                     std::cin >> index;
-                    m_matrixStorage[index].printMatrix();
+                    if (index >= std::ssize(m_matrixStorage))
+                        std::cout << "Invalid index. \n";
+                    else
+                        m_matrixStorage[index].printMatrix();
                 }
                 break;
+            }
+            case 3: {
+                return;
             }
             default:
                 std::cout << "Invalid input. \n";
@@ -91,7 +98,65 @@ void Application::create() {
         }
     }
 }
-void Application::add() {};
+
+void Application::add() {
+    while (true) {
+        int inp{};
+        std::cout << "[1] Add matrices \n"
+                     "[2] Subtract matrices \n"
+                     "[3] Return \n";
+        std::cin >> inp;
+        switch (inp) {
+            case 1:
+            case 2: {
+                if (std::ssize(m_matrixStorage)<=1) {
+                    std::cout << "You must have at least 2 matrices in storage to add. \n";
+                    break;
+                }
+                else {
+                    int ind1{};
+                    int ind2{};
+                    std::cout << "Input index of first matrix. \n";
+                    std::cin >> ind1;
+                    std::cout << "Input index of second matrix. \n";
+                    std::cin >> ind2;
+                    if (ind1 < 0 || ind1 >= std::ssize(m_matrixStorage) || ind2 < 0 || ind2 >= std::ssize(m_matrixStorage)) {
+                        std::cout << "Invalid index entered.\n";
+                        break;
+                    }
+                    if (inp == 1) {
+                        try {
+                            Matrix sum = m_matrixStorage[ind1].addMatrix(m_matrixStorage[ind2]);
+                            m_matrixStorage.push_back(sum);
+                            sum.printMatrix();
+                            std::cout << "Added to index " << std::ssize(m_matrixStorage) - 1 << '\n';
+                        } catch (const std::invalid_argument& e) {
+                            std::cout << "Error: " << e.what() << '\n';
+                        }
+                    }
+                    else {
+                        try {
+                            Matrix diff = m_matrixStorage[ind1].subtractMatrix(m_matrixStorage[ind2]);
+                            m_matrixStorage.push_back(diff);
+                            diff.printMatrix();
+                            std::cout << "Added to index " << std::ssize(m_matrixStorage) - 1 << '\n';
+                        } catch (const std::invalid_argument& e) {
+                            std::cout << "Error: " << e.what() << '\n';
+                        }
+                    }
+                }
+
+            }
+            case 3:
+                return;
+            default:
+                std::cout << "Invalid input.\n";
+                break;
+
+
+        };
+    }
+}
 void Application:: mult() {};
 void Application::scalar() {};
 void Application::transpose() {};
--- Matrix.cpp
+++ Matrix.cpp
@@ -19,50 +19,51 @@ void Matrix::printMatrix() const{
 
  Matrix Matrix::addMatrix(const Matrix& matrix2) const{
     std::vector<std::vector<double>> matrixSum {};
-    if (m_matrix.size() == matrix2.m_matrix.size()) {
-        if (m_matrix[0].size() == matrix2.m_matrix[0].size()) {
-            for (std::ptrdiff_t i{}; i < std::ssize(m_matrix); ++i) {
-                std::vector<double> tempVect{};
-                for (std::ptrdiff_t j{}; j < std::ssize(m_matrix[i]); ++j) {
-                    tempVect.push_back(m_matrix[i][j] + matrix2.m_matrix[i][j]);
-                }
-                matrixSum.push_back(tempVect);
-            }
+    if (m_matrix.size() != matrix2.m_matrix.size() || m_matrix[0].size() != matrix2.m_matrix[0].size()) {
+        throw std::invalid_argument("Matrix dimensions must match for addition. \n");
+    }
+    for (std::ptrdiff_t i{}; i < std::ssize(m_matrix); ++i) {
+        std::vector<double> tempVect{};
+        for (std::ptrdiff_t j{}; j < std::ssize(m_matrix[i]); ++j) {
+            tempVect.push_back(m_matrix[i][j] + matrix2.m_matrix[i][j]);
         }
+        matrixSum.push_back(tempVect);
     }
     return Matrix{matrixSum};
 }
 
  Matrix Matrix::subtractMatrix(const Matrix& matrix2) const{
     std::vector<std::vector<double>> matrixSum {};
-    if (m_matrix.size() == matrix2.m_matrix.size()) {
-        if (m_matrix[0].size() == matrix2.m_matrix[0].size()) {
-            for (std::ptrdiff_t i{}; i < std::ssize(m_matrix); ++i) {
-                std::vector<double> tempVect{};
-                for (std::ptrdiff_t j{}; j < std::ssize(m_matrix[i]); ++j) {
-                    tempVect.push_back(m_matrix[i][j] - matrix2.m_matrix[i][j]);
-                }
-                matrixSum.push_back(tempVect);
-            }
-        }
+    if (m_matrix.size() != matrix2.m_matrix.size() || m_matrix[0].size() != matrix2.m_matrix[0].size()) {
+        throw std::invalid_argument("Matrix dimensions must match for subtraction. \n");
     }
+
+    for (std::ptrdiff_t i{}; i < std::ssize(m_matrix); ++i) {
+        std::vector<double> tempVect{};
+        for (std::ptrdiff_t j{}; j < std::ssize(m_matrix[i]); ++j) {
+            tempVect.push_back(m_matrix[i][j] - matrix2.m_matrix[i][j]);
+        }
+        matrixSum.push_back(tempVect);
+    }
+
     return Matrix{matrixSum};
 }
 
  Matrix Matrix::multiplyMatrix(const Matrix& matrix2) const {
     std::vector<std::vector<double>> newMatrix{};
-    if (m_matrix[0].size() == matrix2.m_matrix.size()) {
-        for (std::ptrdiff_t i{}; i < std::ssize(m_matrix); ++i) {
-            std::vector<double> tempVect{};
-            for (std::ptrdiff_t j{}; j < std::ssize(matrix2.m_matrix[0]); ++j) {
-                double sums{};
-                for (std::ptrdiff_t k{}; k < std::ssize(m_matrix[0]); ++k)
-                    sums+=(m_matrix[i][k] * matrix2.m_matrix[k][j]);
-                tempVect.push_back(sums);
-            }
-            newMatrix.push_back(tempVect);
-
+    if (m_matrix[0].size() != matrix2.m_matrix.size()) {
+        throw std::invalid_argument("Cols in first matrix must match rows in second for multiplication. \n");
+    }
+    for (std::ptrdiff_t i{}; i < std::ssize(m_matrix); ++i) {
+        std::vector<double> tempVect{};
+        for (std::ptrdiff_t j{}; j < std::ssize(matrix2.m_matrix[0]); ++j) {
+            double sums{};
+            for (std::ptrdiff_t k{}; k < std::ssize(m_matrix[0]); ++k)
+                sums+=(m_matrix[i][k] * matrix2.m_matrix[k][j]);
+            tempVect.push_back(sums);
         }
+        newMatrix.push_back(tempVect);
+
     }
     return newMatrix;
 }
